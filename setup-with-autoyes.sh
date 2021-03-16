534b9eec3694e132bf2ab323f62fea4b6d9fc57a
OverdueWeevil2 <61hasanabdullahuygun@gmail.com>
2021-03-16 16:32:43 +0300

    Create setup-with-autoyes.sh

---
+++ setup-with-autoyes.sh
@@ -0,0 +1,18 @@
+sudo apt update
+sudo apt install xfwm4 -y
+sudo apt install xfce4-panel -y
+sudo apt install xfdesktop -y
+sudo apt install xfce4-session -y
+sudo apt install xfce4-settings -y
+sudo apt install thunar -y
+sudo apt install xfce4-appfinder -y
+sudo apt install xfce4-power-manager -y
+sudo apt install libxfce4ui -y
+sudo apt install xfconf -y
+sudo apt install terminal -y
+sudo apt install xfburn -y
+sudo apt install ristretto -y
+sudo apt install xfce4-screensaver -y
+sudo apt install xfce4-screenshooter -y
+sudo apt install mousepad -y
+sudo apt install xfce4-notifyd -y
