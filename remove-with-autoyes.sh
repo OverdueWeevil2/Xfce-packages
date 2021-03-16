c22ddd201a9ee587e7f2d18eb72a63a8cea0f3e1
OverdueWeevil2 <61hasanabdullahuygun@gmail.com>
2021-03-16 16:43:56 +0300

    Create remove-with-autoyes.sh

---
+++ remove-with-autoyes.sh
@@ -0,0 +1,18 @@
+sudo apt update
+sudo apt remove xfwm4 -y
+sudo apt remove xfce4-panel -y
+sudo apt remove xfdesktop -y
+sudo apt remove xfce4-session -y
+sudo apt remove xfce4-settings -y
+sudo apt remove thunar -y
+sudo apt remove xfce4-appfinder -y
+sudo apt remove xfce4-power-manager -y
+sudo apt remove libxfce4ui -y
+sudo apt remove xfconf -y
+sudo apt remove terminal -y
+sudo apt remove xfburn -y
+sudo apt remove ristretto -y
+sudo apt remove xfce4-screensaver -y
+sudo apt remove xfce4-screenshooter -y
+sudo apt remove mousepad -y
+sudo apt remove xfce4-notifyd -y
