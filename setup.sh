bc311b394a1309e4e42761c266c77f9091d21959
OverdueWeevil2 <61hasanabdullahuygun@gmail.com>
2021-04-25 20:20:22 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,18 @@
 sudo apt update
-sudo apt install xfwm4
-sudo apt install xfce4-panel
-sudo apt install xfdesktop
-sudo apt install xfce4-session
-sudo apt install xfce4-settings
-sudo apt install thunar
-sudo apt install xfce4-appfinder
-sudo apt install xfce4-power-manager
-sudo apt install libxfce4ui
-sudo apt install xfconf
-sudo apt install terminal
-sudo apt install xfburn
-sudo apt install ristretto
-sudo apt install xfce4-screensaver
-sudo apt install xfce4-screenshooter
-sudo apt install mousepad
-sudo apt install xfce4-notifyd
+sudo apt install --install-recommends xfwm4
+sudo apt install --install-recommends xfce4-panel
+sudo apt install --install-recommends xfdesktop
+sudo apt install --install-recommends xfce4-session
+sudo apt install --install-recommends xfce4-settings
+sudo apt install --install-recommends thunar
+sudo apt install --install-recommends xfce4-appfinder
+sudo apt install --install-recommends xfce4-power-manager
+sudo apt install --install-recommends libxfce4ui
+sudo apt install --install-recommends xfconf
+sudo apt install --install-recommends terminal
+sudo apt install --install-recommends xfburn
+sudo apt install --install-recommends ristretto
+sudo apt install --install-recommends xfce4-screensaver
+sudo apt install --install-recommends xfce4-screenshooter
+sudo apt install --install-recommends mousepad
+sudo apt install --install-recommends xfce4-notifyd
