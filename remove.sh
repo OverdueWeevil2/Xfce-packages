b8b560ec18c9ed4a592fa69cbf119c88016108d9
OverdueWeevil2 <61hasanabdullahuygun@gmail.com>
2021-05-06 18:43:12 +0300

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -1,18 +1,19 @@
 sudo apt update
-sudo apt remove xfwm4
-sudo apt remove xfce4-panel
-sudo apt remove xfdesktop
-sudo apt remove xfce4-session
-sudo apt remove xfce4-settings
-sudo apt remove thunar
-sudo apt remove xfce4-appfinder
-sudo apt remove xfce4-power-manager
-sudo apt remove libxfce4ui
-sudo apt remove xfconf
-sudo apt remove terminal
-sudo apt remove xfburn
-sudo apt remove ristretto
-sudo apt remove xfce4-screensaver
-sudo apt remove xfce4-screenshooter
-sudo apt remove mousepad
-sudo apt remove xfce4-notifyd
+sudo apt remove --purge xfwm4
+sudo apt remove --purge xfce4-panel
+sudo apt remove --purge xfdesktop
+sudo apt remove --purge xfce4-session
+sudo apt remove --purge xfce4-settings
+sudo apt remove --purge thunar
+sudo apt remove --purge xfce4-appfinder
+sudo apt remove --purge xfce4-power-manager
+sudo apt remove --purge libxfce4ui
+sudo apt remove --purge xfconf
+sudo apt remove --purge terminal
+sudo apt remove --purge xfburn
+sudo apt remove --purge ristretto
+sudo apt remove --purge xfce4-screensaver
+sudo apt remove --purge xfce4-screenshooter
+sudo apt remove --purge mousepad
+sudo apt remove --purge xfce4-notifyd
+sudo apt autoremove
